5b5a0fff6066f068d02034e09192c36eecfa55cb
testertest089 <testertest089@gmail.com>
2019-01-11 20:47:34 -0500

    First Story

--- app/src/main/java/com/example/justin/neuroflow_test/FirstStory.java
+++ app/src/main/java/com/example/justin/neuroflow_test/FirstStory.java
@@ -1,7 +1,6 @@
 package com.example.justin.neuroflow_test;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
